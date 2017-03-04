174a710c03a4d7e76292596528bf30b1a8e373d8
José Aniceto <jose.pedro.aniceto@gmail.com>
2017-03-04 17:29:38 +0000

    Can now identify expressions in submission titles

--- bot.py
+++ bot.py
@@ -1,10 +1,34 @@
 import praw
+import time
 
-reddit = praw.Reddit('PortugalOnReddit', user_agent='PortugalOnReddit v0.1')
 
-subreddit = reddit.subreddit('portugal')
+expressions_to_monitor = "benefícios"
 
-for submission in subreddit.hot(limit=2):
-    print(submission.title)  # Output: the submission's title
-    print(vars(submission))
 
+def authenticate():
+    print("Authenticating...")
+    reddit = praw.Reddit('PortugalOnReddit', user_agent='PortugalOnReddit v0.1')
+    print("Authenticated as {}".format(reddit.user.me()))
+    return reddit
+
+
+def run_bot(reddit):
+    for submission in reddit.subreddit('portugal').hot(limit=25):
+        title = submission.title  # Output: the submission's title
+        # print(title)
+
+        if expressions_to_monitor in title.lower():
+            print(title)
+
+    # Interval between runs
+    time.sleep(10)
+
+
+def main():
+    reddit = authenticate()
+    # while True:
+    run_bot(reddit)
+
+
+if __name__ == '__main__':
+    main()
