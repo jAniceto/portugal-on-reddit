fa3ec0d6c23e9dbfede3b828f8579c4aac02a4c7
José Aniceto <jose.pedro.aniceto@gmail.com>
2017-03-06 00:05:08 +0000

    Working search bot
    Searches for mentions of specific expressions and repost them to a new
    subreddit

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ import pprint
 
 # expressions_to_monitor = ["portugal", "portuguese", "porto", "lisboa", "oporto", "lisbon"]
 expressions_to_monitor = ["portugal"]
-subreddits_to_monitor = "all"
-required_score = 100
+subreddits_to_monitor = "all-portugal-PORTUGALCARALHO-PrimeiraLiga"
+required_score = 50
 
 
 def authenticate():
@@ -21,14 +21,31 @@ def run_bot(reddit):
     for expression in expressions_to_monitor:
         search_query = 'title:' + expression
 
-        for submission in reddit.subreddit(subreddits_to_monitor).search(search_query, syntax='lucene', time_filter='week'):
+        for submission in reddit.subreddit(subreddits_to_monitor).search(search_query, syntax='lucene', time_filter='month'):
             # pprint.pprint(vars(submission))
             if submission.score >= required_score:
-                title = submission.title  # Output the submission's title
-                print(title + '\n')
+                title = submission.title  # Submission's title
+                url = submission.url  # Submission's url
+                xpost = "[{}] ".format(submission.subreddit.display_name)  # x-post string: [subreddit]
+                comments_url = 'https://www.reddit.com' + submission.permalink  # link to submission's comment section
+
+                new_post_title = xpost + title
+                new_post_url = url
+                new_post_text = "[Link to original post here]({})".format(comments_url)
+                post_to = reddit.subreddit('PortugalOnReddit')
+
+                print(new_post_title + ' - ' + url + '\n' + comments_url + '\n')
+
+                new_post(post_to, new_post_title, new_post_url, new_post_text)
+
                 submissions_number += 1
 
-    print(submissions_number)
+    print(str(submissions_number) + ' submissions found')
+
+
+def new_post(subreddit, title, url, text):
+    post = subreddit.submit(title, url=url)
+    sticky_comment = post.reply(text).mod.distinguish(sticky=True)
 
 
 def main():
