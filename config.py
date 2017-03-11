3bf804aeec0f6301a3978a44d3022c4971ac0ca6
José Aniceto <jose.pedro.aniceto@gmail.com>
2017-03-11 23:11:35 +0000

    Extracted bot options to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# BOT CONFIGURATION OPTIONS
+
+# Expressions to monitor for:
+expressions_to_monitor = ['portugal', 'porto', 'oporto', 'lisboa', 'lisbon', 'psbattle']
+
+# Subreddits to monitor for (+ to monitor more multiple subreddits; - to exclude a subreddit):
+subreddits_to_monitor = 'all-portugal-PORTUGALCARALHO-PrimeiraLiga'
+
+# Required score to cross-post a submission found by search when setting up your subreddit:
+required_score = 500
+
+# Required number of comments to cross-post a submission:
+required_comments = 1000
